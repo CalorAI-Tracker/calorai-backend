133b10e732d826d08b8224a8b8d343adb8bcb902
Aleksandra Rykova <alexandra.rykova@uno-soft.ru>
2025-11-04 20:17:49 +0300

    Fixed jwt-auth

--- security/src/main/java/ru/calorai/repository/UserRepository.java
+++ security/src/main/java/ru/calorai/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package ru.calorai.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.calorai.model.UserEntity;
@@ -8,5 +9,9 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    @EntityGraph(attributePaths = {"roles"})
     Optional<UserEntity> findByEmail(String email);
+
+    @EntityGraph(attributePaths = {"roles"})
+    Optional<UserEntity> findById(Long id);
 }
