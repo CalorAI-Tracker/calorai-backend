951a0a66cb1356219b0926d7afc17b97c5ddbf42
Aleksandra Rykova <alexandra.rykova@uno-soft.ru>
2025-11-04 15:05:06 +0300

    Commited

--- security/src/main/java/ru/calorai/config/SecurityConfiguration.java
+++ security/src/main/java/ru/calorai/config/SecurityConfiguration.java
@@ -1,6 +1,10 @@
 package ru.calorai.config;
 
 
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
