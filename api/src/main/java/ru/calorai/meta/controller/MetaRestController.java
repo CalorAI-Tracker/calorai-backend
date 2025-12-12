eac8b22be0b151c996009e0d8146d22bc656b9cc
Aleksandra Rykova <alexandra.rykova@uno-soft.ru>
2025-12-12 20:05:03 +0300

    [#37] Fixed

--- api/src/main/java/ru/calorai/meta/controller/MetaRestController.java
+++ api/src/main/java/ru/calorai/meta/controller/MetaRestController.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/meta")
 @RequiredArgsConstructor
+@PreAuthorize("isAuthenticated()")
 @Tag(name = "Meta", description = "Справочные данные")
 public class MetaRestController {
 
